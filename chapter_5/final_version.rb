dd9d6590952537c49d20dc37714ea08089f169a3
emil <gmrash@gmail.com>
2022-10-25 17:17:40 +0500

    done

--- chapter_5/final_version.rb
+++ chapter_5/final_version.rb
@@ -1,67 +1,109 @@
 class Bottles
   def song
-    verses(99,0)
+    verses(99, 0)
   end
 
   def verses(upper, lower)
-    upper.downto(lower).collect {|i| verse(i)}.join("\n")
+    upper.downto(lower).collect { |i| verse(i) }.join("\n")
   end
 
   def verse(number)
-    bottle_number = BottleNumber.new(number)
-    next_bottle_number = BottleNumber.new(bottle_number.successor)
+    bottle_number = BottleNumber.for(number)
 
-    "#{bottle_number.quantity.capitalize} #{bottle_number.container} of beer on the wall, " +
-    "#{bottle_number.quantity} #{bottle_number.container} of beer.\n" +
-    "#{bottle_number.action}, " +
-    "#{next_bottle_number.quantity} #{next_bottle_number.container} of beer on the wall.\n"
+    "#{bottle_number} of beer on the wall, ".capitalize +
+      "#{bottle_number} of beer.\n" +
+      "#{bottle_number.action}, " +
+      "#{bottle_number.successor} of beer on the wall.\n"
   end
 end
 
 class BottleNumber
   attr_reader :number
 
+  def to_s
+    "#{self.quantity} #{self.container}"
+  end
+
+  def initialize(number)
+    @number = number
+  end
+
+  def self.for(number)
+    case number
+    when 0
+      BottleNumber0
+    when 1
+      BottleNumber1
+    when 6
+      BottleNumber6
+    else
+      BottleNumber
+    end.new(number)
+  end
+
+  def quantity
+    number.to_s
+  end
+
+  def container
+    "bottles"
+  end
+
+  def action
+    "Take #{pronoun} down and pass it around"
+  end
+
+  def pronoun
+    "one"
+  end
+
+  def successor
+    BottleNumber.for(number - 1)
+  end
+end
+
+class BottleNumber0 < BottleNumber
+  attr_reader :number
+
+  def action
+    "Go to the store and buy some more"
+  end
+
   def initialize(number)
     @number = number
   end
 
   def quantity
-    if number == 0
-      "no more"
-    else
-      number.to_s
-    end
-  end
-
-  def container
-    if number == 1
-      "bottle"
-    else
-      "bottles"
-    end
-  end
-
-  def action
-    if number == 0
-      "Go to the store and buy some more"
-    else
-      "Take #{pronoun} down and pass it around"
-    end
-  end
-
-  def pronoun
-    if number == 1
-      "it"
-    else
-      "one"
-    end
+    "no more"
   end
 
   def successor
-    if number == 0
-      99
-    else
-      number - 1
-    end
+    BottleNumber.for(99)
+  end
+end
+
+class BottleNumber1 < BottleNumber
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def container
+    "bottle"
+  end
+
+  def pronoun
+    "it"
+  end
+end
+
+class BottleNumber6 < BottleNumber
+  def quantity
+    1
+  end
+
+  def container
+    "six-pack"
   end
 end
